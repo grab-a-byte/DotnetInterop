52148899ab781c20a39b8b510a48587b7e39679f
parkeradam <30392071+parkeradam@users.noreply.github.com>
2020-03-01 21:27:50 +0000

    Added C# console project and Visual basic DLL

---
+++ CSharpConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using VBClassLib;
+
+namespace CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyVBClass myVBClass = new MyVBClass();
+            var vbResult = myVBClass.AddTwo(1, 2);
+            Console.WriteLine(vbResult);
+        }
+    }
+}
---
+++ VBClassLib/MyVBClass.vb
@@ -0,0 +1,7 @@
+Public Class MyVBClass
+
+    Function AddTwo(A As Integer, B As Integer) As Integer
+        Return A + B
+    End Function
+
+End Class
